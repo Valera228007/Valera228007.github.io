1996917ac448be4ec6b6f91bf4badd470a25aafc
Valera228007 <79106133+Valera228007@users.noreply.github.com>
2021-02-16 14:24:30 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+
+setTimeout(function(){
+    var prelodaer = document.getElementById('page-preloader');
+    if(!prelodaer.classList.contains('done'))
+    {
+        prelodaer.classList.add('done');
+    }
+}, 300);
+function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Введите,пожалуйста свое имя",
+                phone: "Введите свой номер телефона",
+                email: {
+                  required: "Введите свой email ",
+                  email: "Неправильний email"
+                }
+              }
+        });
+        }
+validateForms('.consultation__form');
+validateForms('.questions__form');
+validateForms('.questions__descrForm'); 
+
+
+ const slider = tns({
+     container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+     speed: 1300,
+   });
+ document.querySelector('.prev').addEventListener('click',function () {
+     slider.goTo('prev');
+ });
+ document.querySelector('.next').addEventListener('click',function () {
+     slider.goTo('next');
+ });
+
+let menuBtn = document.querySelector('.hamburger');
+let menu = document.querySelector('.delivery__tabs');
+
+menuBtn.addEventListener('click', function(){
+    menuBtn.classList.toggle('hamburger_active');
+    menu.classList.toggle('delivery__tabs_active');
+});
+$(function(){
+        $("a[href^='#']").click(function(){
+                var _href = $(this).attr("href");
+                $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+                return false;
+        });
+});
